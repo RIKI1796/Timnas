379eb1e16be6348a7c3ee2be6a1b1a6a09eb6b2d
Hafiz 17 <119962175+RIKI1796@users.noreply.github.com>
2024-08-17 18:33:16 +0700

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,12 @@
+function login() {
+         var jawaban = document.getElementById('jawaban').value;
+         var wrong = document.getElementById('wrong');
+  
+         if (jawaban === 'Timnas Indonesia') {
+           location.href = "Timnas.html";
+        } else {
+            alert ('Jawaban Salah!')
+            location.href="login.html";
+        }
+   }
+         console.log("Menyala Abangkuuu!!🔥🔥🔥🔥");
