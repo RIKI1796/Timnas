8b05daf6d85bf527657c037dee1fbca67a46a8f0
Hafiz 17 <119962175+RIKI1796@users.noreply.github.com>
2024-08-22 13:33:08 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ function login() {
            location.href = "/Timnas.html";
         } else {
             alert ('Jawaban Salah!')
-            document.location.href = "/"
+            location.href = "/"
         }
    }
          console.log("Menyala Abangkuuu!!🔥🔥🔥🔥");
