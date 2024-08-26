d8e3159446034ba275776dc7242ce649f237198b
Hafiz 17 <119962175+RIKI1796@users.noreply.github.com>
2024-08-26 15:05:16 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,9 +4,6 @@ function login() {
   
          if (jawaban === 'Timnas Indonesia') {
            window.location.href = "/Timnas.html";
-         } else {
-                  alert ("Jawaban Salah!")
-                  window.location.href = "/"
          };
    }
          console.log("Menyala Abangkuuu!!🔥🔥🔥🔥");
